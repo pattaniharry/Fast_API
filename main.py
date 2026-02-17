e33906ad7c98ecc1c78fa766ae55fa52df56a713
harry pattani <harrypattani15@gmail.com>
2026-02-17 17:13:32 +0530

    parameters query parameters and long query parameters in url

--- main.py
+++ main.py
@@ -1,3 +1,48 @@
-import fastapi
+from fastapi import FastAPI ,Request
+from mock_data import products
+app = FastAPI()
 
-print(fastapi.__version__)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/home")
+def house(): 
+    return {"home":"sweet Home"}
+
+
+@app.get("/contact")
+def contact():
+    return {"contact:you can connect us here"}
+
+
+@app.get("/products")
+def get_products():
+    return products
+
+# handling path parameters  
+
+@app.get("/products/{id}")
+def get_one_product(id:int):
+     for product in products:
+        if product["id"] == id:
+            return product
+     return {"error": "Product not found"}
+ 
+
+#handling query parameter 
+
+@app.get("/greet")
+def greet_name(name:str):
+    return{"message": f"hello {name}"}
+
+
+#handling query parameter with large amount of params 
+#we can do that using a request 
+
+@app.get("/longGreet")
+def greet_user(request:Request):
+    name = request.query_params.get("name")
+    age = request.query_params.get("age")
+    city = request.query_params.get("city")
+    return {"message": f"hello {name} from {city} who is {age} years old"}
---
+++ mock_data.py
@@ -0,0 +1,14 @@
+products = [
+    {
+        "id":1,
+        "title":"Laptop",
+        "price":"1000",
+        "count":10
+    },
+    {
+        "id":2,
+        "title":"phone",
+        "price":"500",
+        "count":4
+    }
+]
